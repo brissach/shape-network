7444132534796484c82328bf34f09cf666a86c12
Clouke <76826179+Clouke@users.noreply.github.com>
2024-01-03 21:06:44 +0100

    added documentation

--- src/main/java/gg/clouke/ContextVector.java
+++ src/main/java/gg/clouke/ContextVector.java
@@ -5,16 +5,30 @@ import java.util.List;
 import java.util.Optional;
 
 /**
+ * A vector class, used for performing operations on vectors
+ *
  * @author Clouke
  * @since 03.01.2024 20:06
  * © shape-network - All Rights Reserved
  */
 public class ContextVector {
 
+  /**
+   * Creates a new vector instance
+   *
+   * @return Returns a new vector instance
+   */
   public static ContextVector newVector() {
     return new ContextVector();
   }
 
+  /**
+   * Creates a new vector instance
+   *
+   * @param size The amount of values to fill the vector with
+   * @param value The value to fill the vector with
+   * @return Returns a new vector instance
+   */
   public static ContextVector fill(int size, double value) {
     ContextVector vector = newVector();
     for (int i = 0; i < size; i++)
@@ -22,6 +36,12 @@ public class ContextVector {
     return vector;
   }
 
+  /**
+   * Sums the dimensions of the given vectors
+   *
+   * @param contexts The vectors to sum the dimensions of
+   * @return Returns the sum of the dimensions of the given vectors
+   */
   public static int summationOf(ContextVector... contexts) {
     int dimensions = 0;
     for (ContextVector context : contexts)
@@ -35,25 +55,56 @@ public class ContextVector {
     for (double value : values) vector.add(value);
   }
 
+  /**
+   * Gets the value at the given index
+   *
+   * @param index The index to get the value at
+   * @return Returns the value at the given index
+   */
   public double get(int index) {
     return vector.get(index);
   }
 
+  /**
+   * Folds the value into an optional
+   *
+   * @param index The index to fold the value at
+   * @return Returns an optional containing the value at the given index
+   */
   public Optional<Double> fold(int index) {
     return index < dimension() ? Optional.of(get(index)) : Optional.empty();
   }
 
+  /**
+   * Adds the given value to the vector
+   *
+   * @param value The value to add to the vector
+   * @return Returns this instance for chaining
+   */
   public ContextVector addValueToVector(double value) {
     vector.add(value);
     return this;
   }
 
+  /**
+   * Adds the given values to the vector
+   *
+   * @param values The values to add to the vector
+   * @return Returns this instance for chaining
+   */
   public ContextVector addValuesToVector(double... values) {
     for (double value : values)
       vector.add(value);
     return this;
   }
 
+  /**
+   * Cuts the vector from the given index to the given index
+   *
+   * @param from The index to start cutting from
+   * @param to The index to cut to
+   * @return Returns a new vector instance of the cut vector
+   */
   public ContextVector cut(int from, int to) {
     ContextVector context = new ContextVector();
     for (int i = from; i < to; i++)
@@ -61,35 +112,82 @@ public class ContextVector {
     return context;
   }
 
+  /**
+   * Cuts the vector from the given index to the end of the vector
+   *
+   * @param from The index to start cutting from
+   * @return Returns a new vector instance of the cut vector
+   */
   public ContextVector cut(int from) {
     return cut(from, dimension());
   }
 
+  /**
+   * Modifies the value at the given index
+   *
+   * @param index The index to modify the value at
+   * @param value The value to modify the value at the given index with
+   * @return Returns this instance for chaining
+   */
   public ContextVector modify(int index, double value) {
     vector.set(index, value);
     return this;
   }
 
+  /**
+   * Multiplies the value at the given index with the given value
+   *
+   * @param index The index to multiply the value at
+   * @param value The value to multiply the value at the given index with
+   * @return Returns this instance for chaining
+   */
   public ContextVector multiply(int index, double value) {
     vector.set(index, vector.get(index) * value);
     return this;
   }
 
+  /**
+   * Divides the value at the given index with the given value
+   *
+   * @param index The index to divide the value at
+   * @param value The value to divide the value at the given index with
+   * @return Returns this instance for chaining
+   */
   public ContextVector divide(int index, double value) {
     vector.set(index, vector.get(index) / value);
     return this;
   }
 
+  /**
+   * Adds the given value to the value at the given index
+   *
+   * @param index The index to add the value at
+   * @param value The value to add to the value at the given index
+   * @return Returns this instance for chaining
+   */
   public ContextVector add(int index, double value) {
     vector.set(index, vector.get(index) + value);
     return this;
   }
 
+  /**
+   * Subtracts the given value from the value at the given index
+   *
+   * @param index The index to subtract the value at
+   * @param value The value to subtract from the value at the given index
+   * @return Returns this instance for chaining
+   */
   public ContextVector subtract(int index, double value) {
     vector.set(index, vector.get(index) - value);
     return this;
   }
 
+  /**
+   * Merges the given vector with this vector
+   *
+   * @param context The vector to merge with this vector
+   * @return Returns this instance for chaining
+   */
   public ContextVector merge(ContextVector context) {
     if (context.dimension() != dimension())
       throw new IllegalStateException("Dimension of node x and y must be equal");
@@ -99,6 +197,12 @@ public class ContextVector {
     return this;
   }
 
+  /**
+   * Agglutinates the given vector with this vector
+   *
+   * @param context The vector to agglutinate with this vector
+   * @return Returns this instance for chaining
+   */
   public ContextVector agglutinate(ContextVector context) {
     for (int index = 0; index < context.dimension(); index++) {
       double x = get(index);
@@ -109,6 +213,12 @@ public class ContextVector {
     return this;
   }
 
+  /**
+   * Performs cosine similarity on the given vector
+   *
+   * @param y The vector to perform cosine similarity on
+   * @return Returns the cosine similarity of the given vector
+   */
   public double cosine(ContextVector y) {
     double dotProduct = 0.0;
     double normX = 0.0;
@@ -129,10 +239,20 @@ public class ContextVector {
     return dotProduct / (magnitudeX * magnitudeY);
   }
 
+  /**
+   * Gets the size of the vector
+   *
+   * @return Returns the size of the vector
+   */
   public int dimension() {
     return vector.size();
   }
 
+  /**
+   * Transforms the vector into an array
+   *
+   * @return Returns the transformed vector
+   */
   public double[] transform() {
     int dim = dimension();
     double[] transformed = new double[dim];
@@ -141,6 +261,12 @@ public class ContextVector {
     return transformed;
   }
 
+  /**
+   * Normalizes the given features
+   *
+   * @param features The features to normalize
+   * @return Returns the normalized features
+   */
   public List<Double> normalize(List<Double> features) {
     List<Double> normalized = new ArrayList<>();
     for (int i = 0; i < features.size(); i++) {
